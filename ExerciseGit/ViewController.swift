6e5da48716e2701143afa91f4dc41164477f45fa
ngt123 <ngt123@gmail.com>
2017-05-05 09:10:22 +0700

    Initial Commit

---
+++ ExerciseGit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ExerciseGit
+//
+//  Created by Suchon's Mac on 5/5/2560 BE.
+//  Copyright © 2560 Suchon's Mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
